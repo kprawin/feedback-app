b8a20ac530659394e0390f335b2cacc9edfda7f7
prawin <kprawinmail@gmail.com>
2022-04-12 04:01:14 +0530

    12Apr22 First Code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react";
+import Header from "./Components/Header";
+
+function App() {
+    return (
+        <>
+            <Header text='Hello World!'/>
+        </>
+    );
+}
+
+export default App;
